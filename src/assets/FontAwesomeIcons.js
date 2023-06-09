b8745f7f36361ec3ec4353151c76372f2f5b94b0
kemenike <kanayoemenike37@gmail.com>
2023-06-09 07:39:49 -0700

    Initial Push

---
+++ src/assets/FontAwesomeIcons.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedIn, faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { faX } from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+    faLinkedIn,
+    faSquareGithub,
+    faX
+);
