13baec33c1451d82e5295effb035686838641c98
Kemenike <kanayoemenike37@gmail.com>
2023-06-12 13:22:00 -0700

    Added Font Awesome to project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { faX } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(
+  faLinkedin,
+  faSquareGithub,
+  faX
+);
 
 function App() {
   return (
     <div className="App">
-     <FontAwesomeIcon icon="fa-solid fa-x" />
     </div>
   );
 }
