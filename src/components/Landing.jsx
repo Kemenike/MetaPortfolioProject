9661f5c43e9278f2cf17a2777f8e42c29bc132ae
kemenike <kanayoemenike37@gmail.com>
2023-06-26 02:19:22 -0700

    Finalized SVG of Astronaut.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,23 @@
 import './Landing.css';
-import React from 'react'
+import React from 'react';
+import Sparkles from '../assets/Sparkles';
+import logo_man from '../assets/logo_man.png'
 
 function Landing() {
   return (
     <div className='landing__main'>
-        <h1 className="testing" style={{color: 'white'}}>Landing Page</h1>
+      <div className="text__wrapper">
+        <h3 className='landing__subtitle subtitle'>
+          Welcome to my page. I'm
+        </h3>
+        <h1 className='landing__title title'>
+          Kanayo Emenike
+        </h1>
+      </div>
+      <div className="img__wrapper">
+        <Sparkles />
+        <img className="logoMan" src={logo_man} alt='' />
+      </div>
     </div>
   )
 }
